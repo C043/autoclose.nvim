68bccbb774df002ea688f9b34edb7bdd15ead6d9
Mario Fragnito <mariofragnitoph@gmail.com>
2025-01-31 10:00:07 +0100

    Updates configuration

--- lua/autoclose.lua
+++ lua/autoclose.lua
@@ -12,7 +12,7 @@ local config = {
       ["}"] = { escape = true, close = false, pair = "{}" },
 
       ['"'] = { escape = true, close = true, pair = '""' },
-      ["'"] = { escape = true, close = true, pair = "''" },
+      -- ["'"] = { escape = true, close = true, pair = "''" },
       ["`"] = { escape = true, close = true, pair = "``" },
 
       [" "] = { escape = false, close = true, pair = "  " },
